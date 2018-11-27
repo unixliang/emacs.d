2d905aa126220d4e189101df1d2e6fa996d75ab3
unixliang <junjieliang@qq.com>
2018-11-27 22:36:37 +0800

    fix gtags use high cpu

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -69,51 +69,53 @@
 (add-hook 'c++-mode-hook 'ggtags-mode)
 
 
-;;;;; 全量更新
-(defun gtags-root-dir ()
-  "Returns GTAGS root directory or nil if doesn't exist."
-  (with-temp-buffer
-    (if (zerop (call-process "global" nil t nil "-pr"))
-        (buffer-substring (point-min) (1- (point-max)))
-      nil)))
+;; somthing wrong in gtags-update-single result in high cpu usage
+;; ;;;;; 全量更新
+;; (defun gtags-root-dir ()
+;;   "Returns GTAGS root directory or nil if doesn't exist."
+;;   (with-temp-buffer
+;;     (if (zerop (call-process "global" nil t nil "-pr"))
+;;         (buffer-substring (point-min) (1- (point-max)))
+;;       nil)))
 
-(defun gtags-update ()
-  "Make GTAGS incremental update"
-  (call-process "global" nil nil nil "-u"))
+;; (defun gtags-update ()
+;;   "Make GTAGS incremental update"
+;;   (call-process "global" nil nil nil "-u"))
 
-(defun gtags-update-hook-all ()
-  (when (gtags-root-dir)
-    (gtags-update)))
-;;;;;;; 全量更新结束
+;; (defun gtags-update-hook-all ()
+;;   (when (gtags-root-dir)
+;;     (gtags-update)))
+;; ;;;;;;; 全量更新结束
 
-;;;;;;; 单文件更新
-(defun gtags-update-single(filename)
-  "Update Gtags database for changes in a single file"
-  (interactive)
-  (start-process
-   "update-gtags"
-   "update-gtags"
-   "bash" "-c"
-   (concat "cd " (gtags-root-dir)
-           " ; gtags --single-update "
-           filename )))
+;; ;;;;;;; 单文件更新
+;; (defun gtags-update-single(filename)
+;;   "Update Gtags database for changes in a single file"
+;;   (interactive)
+;;   (start-process
+;;    "update-gtags"
+;;    "update-gtags"
+;;    "bash" "-c"
+;;    (concat "cd " (gtags-root-dir)
+;;            " ; gtags --single-update "
+;;            filename )))
 
-(defun gtags-update-current-file()
-  (interactive)
-  (defvar filename)
-  (setq filename (replace-regexp-in-string (gtags-root-dir) "." (buffer-file-name (current-buffer))))
-  (gtags-update-single filename)
-  (message "Gtags updated for %s" filename))
+;; (defun gtags-update-current-file()
+;;   (interactive)
+;;   (defvar filename)
+;;   (setq filename (replace-regexp-in-string (gtags-root-dir) "." (buffer-file-name (current-buffer))))
+;;   (gtags-update-single filename)
+;;   (message "Gtags updated for %s" filename))
 
-(defun gtags-update-hook()
-  "Update GTAGS file incrementally upon saving a file"
-  (when ggtags-mode
-    (when (gtags-root-dir)
-      (gtags-update-current-file))))
-;;;;;; 单文件更新结束
+;; (defun gtags-update-hook()
+;;   "Update GTAGS file incrementally upon saving a file"
+;;   (when ggtags-mode
+;;     (when (gtags-root-dir)
+;;       (gtags-update-current-file))))
+;; ;;;;;; 单文件更新结束
 
-(add-hook 'after-save-hook #'gtags-update-hook)
-;;;;;;; 更新tags
+;; (add-hook 'after-save-hook #'gtags-update-hook)
+;; ;;;;;;; 更新tags
+
 
 (add-hook 'c-mode-common-hook
           (lambda ()
