d0a6f56f50cb880c6034bd47d6cd78f681f3b330
unixliang <junjieliang@qq.com>
2018-12-16 20:22:53 +0800

    set-exec-path-from-shell-PATH

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -212,3 +212,17 @@
 ;; end overwrite indent in namespace
 
 (provide 'init-local)
+
+
+;; start set-exec-path-from-shell-PATH
+(defun set-exec-path-from-shell-PATH ()
+  "Set up Emacs' `exec-path' and PATH environment variable to match that used by the user's shell.
+
+This is particularly useful under Mac OSX, where GUI apps are not started from a shell."
+  (interactive)
+  (let ((path-from-shell (replace-regexp-in-string "[ \t\n]*$" "" (shell-command-to-string "$SHELL --login -i -c 'echo $PATH'"))))
+    (setenv "PATH" path-from-shell)
+    (setq exec-path (split-string path-from-shell path-separator))))
+
+(set-exec-path-from-shell-PATH)
+;; end set-exec-path-from-shell-PATH
