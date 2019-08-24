9b3527c280a1bae0a2c014b375d96e66369200a7
unixliang <unixliang@tencent.com>
2019-08-24 16:22:59 +0800

    add tabnine

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -247,7 +247,7 @@ This is particularly useful under Mac OSX, where GUI apps are not started from a
 
 
 
-                                        ;(set-default-font "Monaco 18")
+                                        (set-default-font "Monaco 13")
 
 
 (require 'magit)
@@ -255,3 +255,36 @@ This is particularly useful under Mac OSX, where GUI apps are not started from a
 
 
 (global-set-key (kbd "C-r") 'revert-buffer)
+
+
+
+(load-theme 'sanityinc-tomorrow-bright t)
+
+;; start switch to last buffer
+(global-set-key (kbd "C-l") 'mode-line-other-buffer)
+;; end switch to last buffer
+
+
+;; start tabnine
+(require 'company-tabnine)
+(add-to-list 'company-backends #'company-tabnine)
+;; Trigger completion immediately.
+(setq company-idle-delay 0)
+
+;; Number the candidates (use M-1, M-2 etc to select completions).
+(setq company-show-numbers t)
+
+;; Use the tab-and-go frontend.
+;; Allows TAB to select and complete at the same time.
+(company-tng-configure-default)
+(setq company-frontends
+      '(company-tng-frontend
+        company-pseudo-tooltip-frontend
+        company-echo-metadata-frontend))
+
+(add-hook 'c-mode-hook 'company-mode)
+(add-hook 'c++-mode-hook 'company-mode)
+(add-hook 'protobuf-mode-hook 'company-mode)
+
+
+;; end tabnine
